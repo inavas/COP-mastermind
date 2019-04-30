e9ed5a2f99d4f1c7c599971be82083443b487830
Inma Navas <inmaculada.navas@mango.com>
2019-04-30 12:52:35 +0200

    init

---
+++ src/test/java/TestMastermindDifferentLetters.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class TestMastermindDifferentLetters {
+
+    @Test
+    public void failing_test(){
+        fail();
+    }
+}
